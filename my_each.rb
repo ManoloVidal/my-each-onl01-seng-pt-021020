90047e26c4671709c405564c68b57ffc23621fe1
Juan Vidal-Ramos <vidalramosjm@gmail.com>
2020-02-19 19:21:48 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -3,7 +3,8 @@ def my_each(collection)
     i = 0 
     while i <= collection.length 
      new_collection << yield(collection[i])
-     
+     when i > collection.length
+      raise "This block should not run!"
     i += 1 
     
 end 
