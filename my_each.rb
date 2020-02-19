090eaf9215665fc48c3688de66f3b52c237fae1f
Juan Vidal-Ramos <vidalramosjm@gmail.com>
2020-02-19 19:20:22 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,7 +2,7 @@ def my_each(collection)
   new_collection = []
     i = 0 
     while i <= collection.length 
-     yield(collection[i])
+     new_collection << yield(collection[i])
     i += 1 
 end 
 
